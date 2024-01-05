d76dd5ae53ae4787e1f85f6bbdf684bf71676b9a
fred <domaine75@gmail.com>
2024-01-05 21:03:18 +0100

    Ajoutez des fichiers projet.

---
+++ CreateDictionary/FormMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace CreateDictionary
+{
+  public partial class FormMain : Form
+  {
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private const char space = ' ';
+
+    private void QuitToolStripMenuItem_Click(object sender, EventArgs e)
+    {
+      Application.Exit();
+    }
+
+    private void ButtonAdd_Click(object sender, EventArgs e)
+    {
+      listBoxWords.Items.Clear();
+      var newWords = new List<string>();
+      foreach (string word in textBoxSource.Text.Split(space))
+      {
+        if (!newWords.Contains(word))
+        {
+          newWords.Add(word);
+        }
+      }
+
+
+    }
+  }
+}
